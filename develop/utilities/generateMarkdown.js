908ef7222e8567ec892d6d0253a7bd65613da406
Grant Zepke <grantzepke@gmail.com>
2020-08-25 17:47:02 -0400

    index update

--- develop/utilities/generateMarkdown.js
+++ develop/utilities/generateMarkdown.js
@@ -2,6 +2,58 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  
+  # **${data.title}**
+
+  ${data.badge}
+
+  ## Description
+
+  ${data.description}
+  ## Table of Contents
+  - [Repository](#Repository)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributors](#Contributors)
+  - [Tests](#Tests)
+  - [Development](#Development)
+  - [Contact Author](#Contact)
+
+  ## Repository
+
+  - [Project Repo](${data.repo})
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${data.license}
+
+  ## Contributors
+
+  ${data.contributors}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Development
+
+  ${data.development}
+
+  ## Contact
+  
+  - <${github.email}>
+  - ${github.name}
+  - [GitHub Profile](${github.profile})
+
 `;
 }
 
--- index.js
+++ index.js
@@ -1,16 +1,98 @@
-// array of questions for user
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
+const markdown = require("./assets/utils/generateMarkdown");
+const axios = require("axios");
 
+
+const questions = [
+   {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username? ",
+      default: "23gzepke",
+   },
+   {
+      type: "input",
+      name: "repo",
+      message: "Provide the link to your repo: ",
+      default: "https://github.com/23gzepke/Readme=Generator",
+   },
+   {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project/repo? ",
+      default: "README.md Generator",
+   },
+   {
+      type: "input",
+      name: "description",
+      message: "Describe your project: ",
+      default: "Generate a good README.md file with a CLI application.",
+   },
+   {
+      type: "input",
+      name: "installation",
+      message: "Provide installation instructions: ",
+      default: "Run npm install",
+   },
+   {
+      type: "input",
+      name: "usage",
+      message: "Describe how to use your project: ",
+      default: "Run node index.js",
+   },
+   {
+      type: "input",
+      name: "license",
+      message: "Provide license/badge link info: ",
+      default: "![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)"
+   },
+   {
+      type: "input",
+      name: "contributors",
+      message: "Who are the contributors? ",
+      default: "Grant Zepke",
+   },
+   {
+      type: "input",
+      name: "tests",
+      message: "Describe the tests: ",
+      default: "Tests were performed, this README.md was created using this tool."
+   },
+   {
+      type: "input",
+      name: "development",
+      message: "Describe future development of this application/repo: ",
+      default: "Add function to generate unique badges based on user input."
+   },
+   {
+      type: "input",
+      name: "badge",
+      message: "Add a unique badge to your README (copy the link here): ",
+      default: ""
+   },
 ];
 
-// function to write README file
 function writeToFile(fileName, data) {
-}
+   fs.writeFile(fileName, data, err => {
+      if (err) { throw err; };
+      console.log("README.md created successfully!");
+   });
+};
 
-// function to initialize program
 function init() {
+   inquirer.prompt(questions).then(data => {
+      axios
+         .get("https://api.github.com/users/" + data.username)
+         .then(res => {
+            const github= {
+               email: res.data.email,
+               name: res.data.name,
+               profile: res.data.html_url,
+            };
+            writeToFile("README.md", markdown(data, github));
+         });
+   });
+};
 
-}
-
-// function call to initialize program
 init();
